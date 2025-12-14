93ba1f65fb5c4da5b424d979e0a7bd1bb2c6c5a2
risboo6909 <ttyv00@gmail.com>
2025-12-15 00:28:36 +0100

    Use one general solver for both parts

--- src/main/kotlin/calendar/Day3.kt
+++ src/main/kotlin/calendar/Day3.kt
@@ -19,28 +19,12 @@ class Day3 : Solver {
         1.0
     )
 
-    private fun part1(input: List<List<Int>>): Int {
-        var total = 0
-        for (line in input) {
-            val firstDigit = line
-                .dropLast(1)
-                .withIndex()
-                .maxBy {(_, value) -> value}
-            val secondDigit = line.subList(firstDigit.index + 1, line.size)
-                .max()
-
-            val number = 10 * firstDigit.value + secondDigit
-            total += number
-        }
-        return total
-    }
-
-    private fun part2(input: List<List<Int>>): Long {
+    private fun solver(input: List<List<Int>>, size: Int): Long {
         var best: Long = 0
         var total: Long = 0
 
         fun helper(line: List<Int>, acc: Long, pos: Int, digit: Int) {
-            if (digit == 12) {
+            if (digit == size) {
                 if (acc > best) {
                     best = acc
                 }
@@ -69,6 +53,14 @@ class Day3 : Solver {
             best = 0
         }
         return total
+    }
+
+    private fun part1(input: List<List<Int>>): Long {
+        return solver(input, 2)
+    }
+
+    private fun part2(input: List<List<Int>>): Long {
+        return solver(input, 12)
     }
 
     override fun run(): List<String> {
