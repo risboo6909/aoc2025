94b82600e0521c88031eec546caf42be82d8125c
risboo6909 <ttyv00@gmail.com>
2025-12-14 14:34:34 +0100

    Simplify GH workflow file definition

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("MainKt")
+}
 
 repositories {
     mavenCentral()
