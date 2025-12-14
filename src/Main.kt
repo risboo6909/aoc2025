19dad36075a825c6db6256b00cf6a7aa5307c8e0
risboo6909 <ttyv00@gmail.com>
2025-12-14 10:45:53 +0100

    Add run time measurment for each solver

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,6 @@
 import calendar.Day1
 import calendar.Day2
+import kotlin.system.measureTimeMillis
 
 fun main() {
 
@@ -8,13 +9,22 @@ fun main() {
         2 to Day2()
     )
 
-    for ((day, solver) in solvers) {
-        println("Day $day:")
-        val results = solver.run()
+    val totalTime = measureTimeMillis {
+        for ((day, solver) in solvers) {
+            println("Day $day:")
 
-        println("  Part 1: ${results[0]}")
-        println("  Part 2: ${results[1]}")
-        println()
+            var results: List<Any>
+            val time = measureTimeMillis {
+                results = solver.run()
+            }
+
+            println("  Part 1: ${results[0]}")
+            println("  Part 2: ${results[1]}")
+            println("  Time: ${time}ms")
+            println()
+        }
     }
+
+    println("Total time: ${totalTime}ms")
 
 }
